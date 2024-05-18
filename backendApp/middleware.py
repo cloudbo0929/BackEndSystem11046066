ecace4d9be70be65a424e33de48c1916b68729b1
leedong <11046083@ntub.edu.tw>
2024-05-19 03:23:02 +0800

    新增 - 攔截器
    1.新增LINE APP驗證功能

--- backendApp/middleware.py
+++ backendApp/middleware.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from backendApp.models import Patient
+from lineIntegrations.module.lineVerify import getLineUserUidByToken, getLineTokenByRequest
 
 class AuthenticationMiddleware:
     def __init__(self, get_response):
@@ -14,5 +15,16 @@ def login_required(view_func):
         if not request.user.is_authenticated:
             return render(request, 'deny.html')
         
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+def line_verify(view_func):
+    def wrapper(request, *args, **kwargs):
+        getLineTokenByRequest(request)
+        access_token = request.session.get('line_access_token')
+        lineUid = getLineUserUidByToken(access_token)
+        patient_id = Patient.getpatientIdByLineUid(lineUid)
+        if patient_id == None:
+            return render(request, 'deny.html')
         return view_func(request, *args, **kwargs)
     return wrapper
