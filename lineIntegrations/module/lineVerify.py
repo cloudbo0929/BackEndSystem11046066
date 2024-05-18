6ef66545f84379dcbcd3939aba9ce45b01604528
leedong <11046083@ntub.edu.tw>
2024-05-19 03:24:28 +0800

    新增 - LINE APP驗證
    1.或取LINE access Token
    2.根據access Token去獲取用戶資訊
    3.將有效的access token保存到session

---
+++ lineIntegrations/module/lineVerify.py
@@ -0,0 +1,39 @@
+import requests
+
+CLIENT_ID = "2005031361"
+CLIENT_SECRET = "1d96fa3d36b1d56deffd17bb0c5c6a1c"
+redirect_uri = "https://os-django.leedong.work/linebot/order"
+token_url = "https://api.line.me/oauth2/v2.1/token"
+profile_url = "https://api.line.me/v2/profile"
+def getLineTokenByRequest(request):
+    try:
+        code = request.GET.get('code')
+        payload = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": redirect_uri,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        }
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        response = requests.post(token_url, data=payload, headers=headers)
+        token_data = response.json()
+        access_token = token_data['access_token']
+        if access_token != None:
+            request.session['line_access_token'] = access_token
+        return access_token
+    except:
+        return None
+
+def getLineUserUidByToken(access_token):
+    profile_headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    profile_response = requests.get(profile_url, headers=profile_headers)
+    profile_data = profile_response.json()
+
+    return profile_data['userId']
