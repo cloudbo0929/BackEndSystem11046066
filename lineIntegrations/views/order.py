efc136cab7b772e682104aa81b1389e883ff9ffc
leedong <11046083@ntub.edu.tw>
2024-05-22 02:14:37 +0800

    修改 - LINEBOT
    1.點餐功能整理套件import

--- lineIntegrations/views/order.py
+++ lineIntegrations/views/order.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from backendApp.models import MainCourse, MealOrderTimeSlot, OrderState, Patient, Order
 from datetime import datetime
+from backendApp.models import MainCourse, MealOrderTimeSlot, OrderState, Patient, Order
 from backendApp.middleware import line_verify
 from lineIntegrations.module.lineVerify import getLineUserUidByToken
 
@@ -13,7 +13,7 @@ from lineIntegrations.module.lineVerify import getLineUserUidByToken
 def getWebPage(request):
     nowTime = datetime.now()
     formatted_time = nowTime.strftime("%H:%M")
-    #formatted_time = "12:00" #測試用
+    #formatted_time = "19:31" #測試用
     nowTimeSlot = MealOrderTimeSlot.find_time_slot(formatted_time)
     nextTimeSlot = MealOrderTimeSlot.find_nearest_time_slot(nowTimeSlot, formatted_time)
     next_time_slot_str = f"{nextTimeSlot.startTime.strftime('%H:%M')} 至 {nextTimeSlot.deadlineTime.strftime('%H:%M')}"
@@ -34,7 +34,6 @@ def getWebPage(request):
             msg = '目前尚未開放點餐'
         else:
             msg = '本時段點餐時間已過'
-        
         return render(request, 'order/unopened.html', {'msg': msg, 'nextTimeSlotName': nextTimeSlot.timeSlot_name, 'nextTimeSlot': next_time_slot_str})
 
     if request.method == 'GET':
@@ -49,3 +48,4 @@ def getWebPage(request):
         return JsonResponse({'message': '訂單已成功提交'}, status=201)
     
     return HttpResponse("Unsupported HTTP method", status=405)
+
